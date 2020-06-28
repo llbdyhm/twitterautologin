5bf86358a90e8ee62ec4b519785eea5cde4ef94c
Subodh Vashistha <subodh@users.noreply.github.com>
2020-06-28 22:54:30 +0530

    a

---
+++ automate.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+username = 'Your username'
+password = 'Your password'
+
+url = 'https://twitter.com/login'
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get(url)
+time.sleep(5)
+driver.find_element_by_name('session[username_or_email]').send_keys(username)
+driver.find_element_by_name('session[password]').send_keys(password)
+driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/form/div/div[3]/div').click()
