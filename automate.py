85cc8b8161ef0d744b3c689ef6ef819f14f642cc
Subodh Vashistha <43090551+vvashu38@users.noreply.github.com>
2020-06-28 22:57:59 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -9,7 +9,7 @@ url = 'https://twitter.com/login'
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get(url)
-time.sleep(5)
+time.sleep(5) # decrease or increase as per your convenience
 driver.find_element_by_name('session[username_or_email]').send_keys(username)
 driver.find_element_by_name('session[password]').send_keys(password)
 driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/form/div/div[3]/div').click()
